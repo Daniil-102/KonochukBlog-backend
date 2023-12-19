9efe33582658c570add27c1ee359bdd82557bda3
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2023-12-19 18:07:06 +0200

    c

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const cors = require('cors');
-const session = require('express-session'); // Move this line up
+const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 const {
@@ -42,9 +42,9 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     console.error('DB connection error:', err);
   });
 
-
 const app = express();
 app.use(express.json());
+
 app.use(
   session({
     secret: 'secret123',
