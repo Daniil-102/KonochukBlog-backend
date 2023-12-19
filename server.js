4ac297a4b7a7dcc63a2e19dbdbe899babe70f4cf
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2023-12-19 09:45:36 +0200

    commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { logout } from "./controllers/UserController.js"
 import session from "express-session"
 
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 5000 })
 .then(() => console.log('DB ok'))
 .catch(er => console.log('DB error', er))
 
