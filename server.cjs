5c0113d03715b9a554bf7c49fe69a3671c003bba
Daniel Kononchuk <kononchuk.daniil@knu.ua>
2023-12-19 20:10:13 +0200

    c

--- server.cjs
+++ server.cjs
@@ -4,7 +4,6 @@ const multer = require('multer');
 const cors = require('cors');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const addUser = require('./operations/add_user');
 
 const {
   registerValidation,
